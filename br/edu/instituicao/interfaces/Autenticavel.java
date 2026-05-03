c3920e780c6ead874490c6d3df972a500ae5934c
Anton-Gabriel-code <antoniothecode@gmail.com>
2026-05-03 16:10:33 -0300

    interfaces

---
+++ br/edu/instituicao/interfaces/Autenticavel.java
@@ -0,0 +1,7 @@
+package br.edu.instituicao.interfaces;
+
+public interface Autenticavel {
+
+    boolean login(String senha);
+    
+}
---
+++ br/edu/instituicao/model/Pessoa.java
@@ -0,0 +1,29 @@
+package br.edu.instituicao.model;
+
+public abstract class Pessoa {
+
+        private String nome;
+    private String cpf;
+    private String email;
+
+    public Pessoa(String nome, String cpf, String email) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+    }
+
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+}
+
+
+
+
+
+
+
+
+
