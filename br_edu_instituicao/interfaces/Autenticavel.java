c120be31ff3cc1d9cf7c9f2395680e1337ecbe4b
Anton-Gabriel-code <antoniothecode@gmail.com>
2026-05-03 14:54:06 -0300

    reorganizando

--- br_edu_instituicao/interfaces/Autenticavel.java
+++
@@ -1,8 +0,0 @@
-package br.edu.instituicao.interfaces;
-
-
-public interface Autenticavel {
-    boolean login(String senha);
-}
-
-// Classe que conterá o método login e outros arquivos que implemnetarem precisam ter o método login(), para que possa acessar
